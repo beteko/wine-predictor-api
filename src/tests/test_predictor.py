5bd2777b4cb5fa3c8b5cce14f512635b80359b7e
beteko <herculive@gmail.com>
2023-01-14 16:25:38 +0100

    test: add unit test

--- src/tests/test_predictor.py
+++ src/tests/test_predictor.py
@@ -1,7 +1,7 @@
 import joblib
 
 
-def test_train_model(app_client, test_auth, test_model_path, mocker):
+def test_estimate_wine_quality(app_client, test_auth, test_model_path, mocker):
     # given
     endpoint = "/wine/quality"
     url_params = "fixed_acidity=4.6&volatile_acidity=0.12&citric_acid=0.0&residual_sugar=0.9&chlorides=0.012&free_sulfur_dioxide=1.0&total_sulfur_dioxide=6.0&density=0.99007&ph=2.74&sulphates=0.33&alcohol=8.4"
