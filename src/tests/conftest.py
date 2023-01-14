7893ffa24875f8791a9917405071b57239e6be3f
Etienne Bognan EKPO <45015267+beteko@users.noreply.github.com>
2023-01-14 16:57:09 +0100

    test: add unit test (#8)
    * test: add unit test

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 import os
+import base64
+from tests.assets import where as asset_dir_path
 
 
 @pytest.fixture()
@@ -7,9 +9,31 @@ def app():
     from wine_predictor_api import create_app
     app = create_app()
     app.config.update(
-        TESTING=True
+        TESTING=True,
+        LOGIN_DISABLED=True
     )
     yield app
+
+
+@pytest.fixture(scope="function")
+def skip_security(mocker):
+    mocker.patch("wine_predictor_api.security.authentication.basic_auth", return_value={"sub": "test_user"})
+
+
+@pytest.fixture
+def test_auth():
+    test_cred = base64.b64encode(b"test_user:test_pass").decode('utf-8')
+    return f"Basic {test_cred}"
+
+
+@pytest.fixture
+def test_data_path():
+    return asset_dir_path() + "/sample_data.csv"
+
+
+@pytest.fixture
+def test_model_path():
+    return asset_dir_path() + "/test_model.jl"
 
 
 @pytest.fixture()
@@ -18,7 +42,7 @@ def app_client(app):
 
 
 @pytest.fixture()
-def app_runner(app):
+def app_runner(app, pytest_configure):
     return app.test_cli_runner()
 
 
