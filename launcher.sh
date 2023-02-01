6cec0e7d5742beea5d819a1d9080c15f96730438
Etienne Bognan EKPO <45015267+beteko@users.noreply.github.com>
2023-02-01 20:24:11 +0100

    Update launcher.sh (#11)

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,8 @@
+export PYTHONPATH="src"
 export API_CONFIG="config.json"
 export FLASK_DEBUG=true
-PORT=8001
 export FLASK_APP="wine_predictor_api:create_app"
 
-lsof -t -i:$PORT | xargs kill -9
+PORT=5000
+
 python -m flask run -h 0.0.0.0 -p $PORT
