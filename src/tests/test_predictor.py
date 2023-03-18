3ffa1681cf354ef27377faf857997d40eea66edf
Etienne Bognan EKPO <45015267+beteko@users.noreply.github.com>
2023-03-18 15:59:01 +0100

    test(conftest): disable test openapi security operation (#12)
    * test: disable security during unit test
    
    * dep: freeze dependencies
    
    * update VERSION

--- src/tests/test_predictor.py
+++ src/tests/test_predictor.py
@@ -1,7 +1,7 @@
 import joblib
 
 
-def test_estimate_wine_quality(app_client, test_auth, test_model_path, mocker):
+def test_estimate_wine_quality(app_client, test_model_path, mocker):
     # given
     endpoint = "/wine/quality"
     url_params = "fixed_acidity=4.6&volatile_acidity=0.12&citric_acid=0.0&residual_sugar=0.9&chlorides=0.012&free_sulfur_dioxide=1.0&total_sulfur_dioxide=6.0&density=0.99007&ph=2.74&sulphates=0.33&alcohol=8.4"
@@ -10,7 +10,7 @@ def test_estimate_wine_quality(app_client, test_auth, test_model_path, mocker):
                  return_value=joblib.load(test_model_path), autospec=True)
 
     # when
-    response = app_client.get(f"{endpoint}?{url_params}", headers={"Authorization": test_auth})
+    response = app_client.get(f"{endpoint}?{url_params}")
 
     # then
     assert response.status_code == 200
